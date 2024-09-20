619cc1d375cd6a9db1fd91b8f863ccd103bbdc9a
sriram2534 <sriram2534.k@gmail.com>
2024-09-19 22:13:21 -0500

    copy code

--- src/com/java/learning/Product.java
+++ src/com/java/learning/Product.java
@@ -1,4 +1,21 @@
 package com.java.learning;
 
 public class Product {
+    private String productName;
+    private double productPrice;
+    private String slin;
+    private String productId;
+    private String categoryId;
+    private String categoryName;
+    private String catalogType;
+
+    public Product(String productName, double productPrice, String slin, String productId, String categoryId, String categoryName, String catalogType) {
+        this.productName = productName;
+        this.productPrice = productPrice;
+        this.slin = slin;
+        this.productId = productId;
+        this.categoryId = categoryId;
+        this.categoryName = categoryName;
+        this.catalogType = catalogType;
+    }
 }
--- src/com/java/learning/Store.java
+++ src/com/java/learning/Store.java
@@ -1,4 +1,88 @@
 package com.java.learning;
 
+import java.util.ArrayList;
+
 public class Store {
+    private String storeId;
+    private String address;
+    private String city;
+    private String state;
+    private String zipCode;
+    private String country;
+    private String phone;
+    private final ArrayList<String> products = new ArrayList<String>();
+
+    public Store(String storeId, String address, String city, String state, String zipCode, String country, String phone) {
+        this.storeId = storeId;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zipCode = zipCode;
+        this.country = country;
+        this.phone = phone;
+    }
+
+    public String getStoreId() {
+        return storeId;
+    }
+
+    public void setStoreId(String storeId) {
+        this.storeId = storeId;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void addProduct(String productId) {
+        products.add(productId);
+    }
+
+    public ArrayList<String> getProducts() {
+        return products;
+    }
 }
